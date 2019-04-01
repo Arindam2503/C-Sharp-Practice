1eafe4dbed7444cfc4059cabf83db2aa21d35de8
arindhsa <arindhsa@LIN86000090>
2019-04-01 15:58:14 +0530

    Add Inheritance

---
+++ CheckInheritance/CheckInheritance/B.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckInheritance
+{
+    class B : A
+    {
+        int b;
+        public B() {
+            Console.WriteLine("Class B: Function B");
+        }
+        public void G() {
+            Console.WriteLine("Class B: Function G");
+        }
+    }
+}
