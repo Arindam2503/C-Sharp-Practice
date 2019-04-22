d44ddb431e58fdcf35ca1162e340c1f9a326b6bb
arindhsa <arindhsa@LIN86000090>
2019-04-22 11:29:38 +0530

    Angular 2

---
+++ CheckInheritance/Attributes/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    class Comment : Attribute
+    {
+        string text, author;
+        public string Text { get { return text; } }
+        public string Author { get { return author; } set { author = value; } }
+        public Comment(string text)
+        {
+            this.text = text; author = "HM";
+        }
+    }
+}
---
+++ CheckInheritance/Attributes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(C);
+            object[] a = t.GetCustomAttributes(typeof(Comment), true);
+            Comment ca = (Comment)a[0];
+            Console.WriteLine(ca.Text + ", " + ca.Author);
+        }
+    }
+}
